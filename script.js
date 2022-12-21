c6050fbeaf945836e9826f99223e6eb72b865078
Jaime Luong <jaime.luong@gmail.com>
2022-12-20 21:00:32 -0600

    Add chaining calculation functionality, change console debugger method

--- script.js
+++ script.js
@@ -26,19 +26,27 @@ const equalButton = document.querySelector('#equal');
 
 // Add event listeners to operator buttons
 plusButton.addEventListener('click', () => {
-    toggle();
+    if(first) {
+        toggle();
+    }
     operator = 'add'; // Set operator to addition
 });
 minusButton.addEventListener('click', () => {
-    toggle();
+    if(first) {
+        toggle();
+    }
     operator = 'subtract'; // Set operator to addition
 });
 multiplyButton.addEventListener('click', () => {
-    toggle();
+    if(first) {
+        toggle();
+    }
     operator = 'multiply'; // Set operator to addition
 });
 divideButton.addEventListener('click', () => {
-    toggle();
+    if(first) {
+        toggle();
+    }
     operator = 'divide'; // Set operator to addition
 });
 
@@ -83,13 +91,25 @@ const division = (num1, num2) => {
 // Operate function will perform calculation
 const operate = (num1, num2, operator) => {
     if(operator === 'add') {
-        display.textContent = addition(parseInt(num1), parseInt(num2));
+        let result = addition(parseInt(num1), parseInt(num2));
+        display.textContent = result;
+        firstOperand = [result];
+        secondOperand = [];
     } else if (operator === 'subtract') {
-        display.textContent = subtraction(num1, num2);
+        let result = subtraction(num1, num2);
+        display.textContent = result;
+        firstOperand = [result];
+        secondOperand = [];
     } else if (operator === 'multiply') {
-        display.textContent = multiplication(num1, num2);
+        let result = multiplication(num1, num2)
+        display.textContent = result;
+        firstOperand = [result];
+        secondOperand = [];
     } else {
-        display.textContent = division(num1, num2);
+        let result = division(num1, num2);
+        display.textContent = result;
+        firstOperand = [result];
+        secondOperand = [];
     }
 }
 
@@ -98,9 +118,8 @@ equalButton.addEventListener('click', () => {
     operate(firstOperand.join(''), secondOperand.join(''), operator);
 });
 
-// Debugging, using clear button for now...
-const clr = document.querySelector('#clear');
-clr.addEventListener('click', () => {
+// Debugging purposes only
+addEventListener('keydown', () => {
     console.log(firstOperand);
     console.log(secondOperand);
     console.log(operator);
